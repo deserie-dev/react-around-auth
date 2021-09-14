cd87fbb929cef3a1fe533379bad2af1046bee82a
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-14 13:44:06 +0200

    add auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,40 @@
+export const BASE_URL = 'https://register.nomoreparties.co';
+
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const authorize = (identifier, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ identifier, password })
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.jwt) {
+        localStorage.setItem('jwt', data.jwt);
+        return data;
+      } else {
+        return;
+      }
+    })
+    .catch((err) => console.log(err));
+};
